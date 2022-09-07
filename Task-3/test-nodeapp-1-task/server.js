105b355074a41cea34e1e8f8bfac6c8fb391d70d
SuhaasNandeesh <suhaasnandeesh@gmail.com>
2022-09-07 23:55:11 +0530

    updated host url

--- Task-3/test-nodeapp-1-task/server.js
+++ Task-3/test-nodeapp-1-task/server.js
@@ -4,13 +4,13 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = 'cp123-lb-253816681.us-east-1.elb.amazonaws.com';
 
 // App
 const app = express();
-app.get('/*', (req, res) => {
+app.get('/app', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log('Running on http://${HOST}:${PORT}');
