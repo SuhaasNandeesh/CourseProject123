8a928309e6a548fd9018ab7abac491104a5d4a6d
SuhaasNandeesh <suhaasnandeesh@gmail.com>
2022-09-08 01:31:09 +0530

    Revert "updated listen command"
    This reverts commit 32ae43ccf2b28d0d257724548918a3d196c48416.
    
    revert due to app error

--- Task-3/test-nodeapp-1-task/server.js
+++ Task-3/test-nodeapp-1-task/server.js
@@ -12,6 +12,5 @@ app.get('/app', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, () => {
-  console.log('Running on http://${HOST}:${PORT}');
-});
+app.listen(PORT, HOST);
+console.log('Running on http://${HOST}:${PORT}');
